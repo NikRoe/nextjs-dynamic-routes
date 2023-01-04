c2f7b20c2f35b913acd22cfac9fe602b7c5cd19d
Niklas Röttger <niklas.roettger@outlook.de>
2023-01-04 14:57:13 +0100

    add dynamic routes demo

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,14 +1,36 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { movies } from "../../lib/data";
+import Head from "next/head";
 
 export default function Movies() {
+  const router = useRouter();
+
   return (
     <>
+      <Head>
+        <title>Movies</title>
+      </Head>
       <h1>List of Movies</h1>
       <ul>
-        <li>
-          <Link href={`/movies/aquaman`}>Aquaman</Link>
-        </li>
+        {movies.map((movie) => {
+          return (
+            <li key={movie.id}>
+              <Link href={`/movies/${movie.slug}`}>{movie.title}</Link>
+            </li>
+          );
+        })}
       </ul>
+      <button
+        type="button"
+        onClick={() => {
+          if (confirm("Are you aquaman?")) {
+            router.push("/movies/aquaman");
+          }
+        }}
+      >
+        Are you aquaman?
+      </button>
     </>
   );
 }
